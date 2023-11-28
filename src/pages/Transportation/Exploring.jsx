26f672171980912bd78fafc334e8f0e6360147d1
Muhammad Ghazanfar <116085344+blackhorsese@users.noreply.github.com>
2023-11-29 04:09:30 +0500

    Add files via upload

---
+++ src/pages/Transportation/Exploring.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export default function Exploring() {
+    return (
+        <>
+            <div className='justify-center mx-auto max-w-screen-2xl md:px-10 px-5 items-center py-10'>
+                <h1 className='md:text-4xl text-2xl font-semibold'>
+                    Keep exploring
+                </h1>
+                <div className='flex md:grid-cols-4 overflow-x-auto mx-auto gap-5 md:gap-10 py-10'>
+                    <a href='/road-trip-essentials' className='justify-center hover:bg-[#f1f1f1] border-[#78006E] border-opacity-20 border rounded-xl shadow-xl'>
+                        <div className='px-3 lg:h-96 h-80 py-4 w-72 lg:w-[22rem]'>
+                            <img className='w-96' src='./2.webp' alt='images'/>
+                            <div className='pt-5 px-5'>
+                                <h1 className='font-semibold pb-5 lg:text-lg text-base'>
+                                    Road trip guide
+                                </h1>
+                                <p className='lg:text-base text-sm pt-3 border-t border-[#78006E] border-opacity-20 hover:text-[#78006E]'>
+                                    Learn More
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                    <a href='/trip-planner' className='justify-center hover:bg-[#f1f1f1] border-[#78006E] border-opacity-20 border rounded-xl shadow-xl'>
+                        <div className='px-3 lg:h-96 h-80 py-4 w-72 lg:w-[22rem]'>
+                        <img className='w-96' src='./3.jpg' alt='images'/>
+                        <div className='pt-5 px-5'>
+                            <h1 className='font-semibold pb-5 lg:text-lg text-base'>
+                                Trip Planner
+                            </h1>
+                            <p className='lg:text-base text-sm pt-3 border-t border-[#78006E] border-opacity-20 hover:text-[#78006E]'>
+                                Learn More
+                            </p>
+                        </div>
+                        </div>
+                    </a>
+                    <a href='/travel-safety-tips' className='justify-center hover:bg-[#f1f1f1] border-[#78006E] border-opacity-20 border rounded-xl shadow-xl'>
+                        <div className='px-3 lg:h-96 h-80 py-4 w-72 lg:w-[22rem]'>
+                        <img className='w-96' src='./4.png' alt='images'/>
+                        <div className='pt-5 px-5'>
+                            <h1 className='font-semibold pb-5 lg:text-lg text-base'>
+                                Travel Safety Advice
+                            </h1>
+                            <p className='lg:text-base text-sm pt-3 border-t border-[#78006E] border-opacity-20 hover:text-[#78006E]'>
+                                Learn More
+                            </p>
+                        </div>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </>
+    )
+}
