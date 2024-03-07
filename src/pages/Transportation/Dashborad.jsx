b971ea1b8e4f292dbb746e33df2eb52bc091d568
Muhammad Ghazanfar <116085344+blackhorsese@users.noreply.github.com>
2024-03-07 06:46:19 +0500

    Update Dashborad.jsx

--- src/pages/Transportation/Dashborad.jsx
+++ src/pages/Transportation/Dashborad.jsx
@@ -5,7 +5,7 @@ export default function Art() {
     <>
         <div className='duration-500'>
             <div className='bg-local md:h-32 h-44'>
-                <img className='z-0 duration-500 w-full object-cover h-[12rem] md:h-[36rem]' src='./plane_your_trip/transportation/transportation.png' alt='images'/>
+                <img className='z-0 duration-500 w-full object-cover h-[12rem] md:h-[36rem]' src='./plane_your_trip/transportation/Transportation.png' alt='images'/>
             </div>
             <div className='md:px-16 mx-auto justify-center overflow-hidden px-5 lg:text-center'>
                 <div className='text-[#000] md:text-white md:py-20 mt-12 md:mt-16 md:pb-0'>
