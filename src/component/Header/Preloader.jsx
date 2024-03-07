64a510fc73b771e18fa5602ad22880a75a140d98
Muhammad Ghazanfar <116085344+blackhorsese@users.noreply.github.com>
2024-03-07 06:18:11 +0500

    Add files via upload

---
+++ src/component/Header/Preloader.jsx
@@ -0,0 +1,12 @@
+// Preloader.js
+import React from 'react';
+
+const Preloader = () => {
+  return (
+    <div className="preloader">
+      <div className="loader"></div>
+    </div>
+  );
+};
+
+export default Preloader;
